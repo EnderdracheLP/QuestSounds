9417fef2905cec0f3a73c7c77c1d9ee9b33020e0
Rugtveit <BenjaminRugtveit@gmail.com>
2020-06-13 15:00:27 +0200

    finished with audiocliploader

--- include/audiocliploader.hpp
+++ include/audiocliploader.hpp
@@ -1,6 +1,10 @@
 #include <dlfcn.h>
 #include "../extern/beatsaber-hook/shared/utils/utils.h"
+#include "../extern/beatsaber-hook/shared/utils/logging.hpp"
+#include "../extern/beatsaber-hook/include/modloader.hpp"
 #include "../extern/beatsaber-hook/shared/utils/typedefs.h"
+#include "../extern/beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+#include "../extern/beatsaber-hook/shared/utils/il2cpp-functions.hpp"
 namespace audioClipLoader
 {
     class loader : Il2CppObject*
@@ -10,10 +14,11 @@ namespace audioClipLoader
             int audioType;
             Il2CppObject* loadedClip; //Audioclip
             Il2CppObject* action;
-            bool loaded;
+            bool loaded = false;
             bool load();
         private: 
             Il2CppObject* audioClipAsync;
-            static void audioClipCompleted(AssetBundle* obj, Il2CppObject* asyncOp);
+            Il2CppObject* audioClipRequest;
+            static void audioClipCompleted(loader* obj, Il2CppObject* asyncOp);
     };
 }
--- src/audiocliploader.cpp
+++ src/audiocliploader.cpp
@@ -1,6 +1,44 @@
 #include "../include/audiocliploader.hpp"
 
-bool audioClipLoader::loader load()
+bool audioClipLoader::loader::load()
 {
+    //TODO figure out how to get logging working here!
+    //Stage 0 
+    bool fileError = fileexists(filePath.c_str());
+    bool error = (audioClipAsync != nullptr || loadedClip != nullptr || !fileexists);
+    if(error)
+    {
+        if(!fileError)
+        {
+            // log file doesn't exist error!
+        } else 
+        {
+            // Log another error!
+        }
+        return false;
+    }
+
+    //Stage 1
+    Il2CppString* filePathStr = il2cpp_utils::createcsstr(filePath);
+    audioClipRequest = CRASH_UNLESS(il2cpp_utils::RunMethod("UnityEngine.Networking", "UnityWebRequestMultimedia", "GetAudioClip", filePath, audioType));
+    audioClipAsync = CRASH_UNLESS(il2cpp_utils::RunMethod(webRequest, "SendWebRequest"));
     
+    //Stage 2
+    const MethodInfo* addCompletedMethod = CRASH_UNLESS(il2cpp_utils::FindMethodUnsafe(audioClipAsync, "add_completed", 1));
+    auto action = CRASH_UNLESS(il2cpp_utils::MakeAction(addCompletedMethod, 0, this, audioClipCompleted));
+    CRASH_UNLESS(il2cpp_utils::RunMethod(audioClipAsync, completedMethod, action));
+    return true;
 }
+
+void audioClipLoader::loader::audioClipCompleted(loader* obj, Il2CppObject* asyncOp)
+{
+    // Stage 1
+    obj->loadedClip = CRASH_UNLESS(il2cpp_utils::RunMethod("UnityEngine.Networking", "DownloadHandlerAudioClip", "GetContent", obj->audioClipRequest));
+    if(obj->loadedClip != nullptr)
+    {
+        obj->loaded = true;
+    }
+    // Finished
+}
+
+
