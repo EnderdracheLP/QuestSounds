6bcb17a5a90b67ffe7bbac17bbfbfba9b1a11af7
Rugtveit <BenjaminRugtveit@gmail.com>
2020-10-12 14:23:15 +0200

    Updated utils and added MakeFolder

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,10 +3,12 @@
 #include <utility>
 #include <dirent.h>
 #include <map>
+#include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
 class FileUtils 
 {
     public: 
         static std::string GetFileExtension(const std::string& FileName);
         static bool GetFileNameWithExtension(std::map<std::string, int> fileWithExtension, std::string directory);
         static std::string GetFileName(const std::string& file);
+        static bool MakeFolder(std::string_view folderPath);
 };
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,5 @@
 #include "include/utils.hpp"
+
 std::string FileUtils::GetFileExtension(const std::string& file)
 {
     if(file.find_last_of(".") != std::string::npos) return file.substr(file.find_last_of("."));
@@ -11,30 +12,40 @@ std::string FileUtils::GetFileName(const std::string& file)
     return "";
 }
 
-bool FileUtils::GetFileNameWithExtension(std::map<std::string, int> fileWithExtension, std::string directory)
+bool FileUtils::GetFileNameWithExtension(std::map<std::string, int> fileWithExtension, std::string dir)
 {
-    bool foundExtension = false; 
-    DIR* fileDir = opendir(directory.data());
+    bool foundSoundFile = false; 
+    DIR* fileDir = opendir(dir.data());
     std::string filetypes[3] = {".mp3", ".ogg", ".wav"};
     struct dirent *files;
     if(fileDir != NULL)
     {
-        while((files = readdir(fileDir)) != NULL)
+        auto readFiles = readdir(fileDir);
+        while(readFiles != NULL)
         {
-            std::string foundExtension = GetFileExtension(files->d_name);
+            std::string foundExtension = GetFileExtension(readFiles->d_name);
             for(int i = 0; i > 3; i++)
             {
                 if(foundExtension == filetypes[i])
                 {
-                    std::string fileName = GetFileName(files->d_name);
+                    std::string fileName = GetFileName(readFiles->d_name);
                     fileWithExtension[fileName] = i; 
-                    foundExtension = true; 
+                    foundSoundFile = true; 
                 }
             }
-                
         }
         closedir(fileDir);
-        if(!foundExtension) return false; 
+        if(!foundSoundFile) return false; 
         return true;
     } else return false;
+}
+
+bool FileUtils::MakeFolder(std::string_view folderPath)
+{
+    if(!direxists(folderPath.data()))
+    {
+        int makePath = mkpath(folderPath.data(), 0700);
+        if(makePath == -1) return false;
+        else return true;
+    } else return true; 
 }
