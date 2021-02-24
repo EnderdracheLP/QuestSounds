48e64b46f1fac96a1dad1dda7ec900a132f25c64
EnderdracheLP <enderdrachelp@gmail.com>
2021-02-24 18:44:09 +0100

    deleted second logging file

--- logging (2).ps1
+++
@@ -1,7 +0,0 @@
-$timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
-adb logcat -T "$timestamp" main-modloader:W QuestHook[QuestSounds`|v0.2.0]:* AndroidRuntime:E *:S QuestHook[UtilsLogger`|v0.8.4]:E QuestHook[UtilsLogger`|v1.0.12]:E
-        if ($args[0] -eq "--debug") {
-            $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
-            adb logcat -T "$timestamp" main-modloader:W QuestHook[TrickSaber`|v0.3.1]:* AndroidRuntime:E *:S QuestHook[UtilsLogger`|v0.8.4]:* QuestHook[UtilsLogger`|v1.0.12]:*
-        }
-echo "Exiting logging.ps1"
