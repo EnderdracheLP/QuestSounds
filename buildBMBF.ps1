0c3f4485b0117f3f99c8dba78506d5afce946bce
EnderdracheLP <enderdrachelp@gmail.com>
2021-03-14 18:02:53 +0100

    Fixed buildBMBF missing bmbfmod.json

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -5,6 +5,6 @@ $ModID = "QuestSounds"
 $BSHook = "1_1_6"
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", -DestinationPath "./$ModID.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\bmbfmod.json" -DestinationPath "./$ModID.zip" -Update
 }
 echo "Task Completed"
