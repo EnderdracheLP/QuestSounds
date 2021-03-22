b8636c8b2b9a55580f58385cf1464ee864ccdd09
EnderdracheLP <enderdrachelp@gmail.com>
2021-03-22 11:50:51 +0100

    Testing Auto packaging of qmod, and BMBF Mod in QuestSounds.

--- buildQP.ps1
+++ buildQP.ps1
@@ -1,11 +1,14 @@
 # Builds a .qmod file for loading with QP
+if ($args.Count -eq 0) {
+echo "Compiling Mod"
 & $PSScriptRoot/build.ps1
+}
 
 $ModID = "QuestSounds"
-$BSHook = "1_1_6"
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
+echo "Packaging QMod with ModID: $ModID"
+    Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
     Remove-Item "./$ModID.qmod"
     Rename-Item "./Temp$ModID.zip" "./$ModID.qmod"
 }
