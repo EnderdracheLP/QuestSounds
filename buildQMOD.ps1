4b54fd12324e36eb8420b79f19bdb32e1d521302
EnderdracheLP <enderdrachelp@gmail.com>
2021-04-12 14:36:12 +0200

    Use bs-hook 1.3.3 now and made dependency updating easier

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -1,21 +1,28 @@
 # Builds a .qmod file for loading with QP
 if ($args.Count -eq 0) {
 $ModID = "QuestSounds"
-$BSHook = "1_2_6"
+$BSHook = "1_3_3"
+$VERSION = "0.3.0"
 echo "Compiling Mod"
 & $PSScriptRoot/build.ps1
 }
+
+    $modjson = "$PSScriptRoot/mod.json"
 
 # TODO: Get the below working with Github Actions variables.
 if ($args[0] -eq "--package") {
 echo "Actions: Packaging QMod with ModID: $ModID"
     $ModID = $env:module_id
     $BSHook = $env:bs_hook
+    (Get-Content $modjson).replace('{VERSION_NUMBER_PLACEHOLDER}', "$env:version") | Set-Content $modjson
+    (Get-Content $modjson).replace('{BS_Hook}', "$BSHook") | Set-Content $modjson
     Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
     Move-Item "./Temp$ModID.zip" "./$ModID.qmod" -Force
 }
 if ($?) {
 echo "Packaging QMod with ModID: $ModID"
+    (Get-Content $modjson).replace('{VERSION_NUMBER_PLACEHOLDER}', "$VERSION") | Set-Content $modjson
+    (Get-Content $modjson).replace('{BS_Hook}', "$BSHook") | Set-Content $modjson
     Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
     Move-Item "./Temp$ModID.zip" "./$ModID.qmod" -Force
 }
