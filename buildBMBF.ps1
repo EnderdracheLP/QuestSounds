41d01fc66e807fe22dffc07f3a72a87255cf8327
EnderdracheLP <enderdrachelp@gmail.com>
2021-04-14 17:09:21 +0200

    Actions for packaging a bmbfmod

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -6,24 +6,22 @@ $VERSION = "0.3.0"
 echo "Compiling Mod"
 & $PSScriptRoot/build.ps1
 }
-
-    $modjson = "$PSScriptRoot/mod.json"
     Copy-Item "./mod_Template.json" "./mod.json" -Force
 # TODO: Get the below working with Github Actions variables.
 if ($args[0] -eq "--package") {
     $ModID = $env:module_id
     $BSHook = $env:bs_hook
 echo "Actions: Packaging QMod with ModID: $ModID and BS-Hook version: $BSHook"
-    (Get-Content "./mod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$env:version") | Set-Content "./mod.json"
-    (Get-Content "./mod.json").replace('{BS_Hook}', "$BSHook") | Set-Content "./mod.json"
-    Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
-    Move-Item "./Temp$ModID.zip" "./$ModID.qmod" -Force
+    (Get-Content "./bmbfmod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$env:version") | Set-Content "./bmbfmod.json"
+    (Get-Content "./bmbfmod.json").replace('{BS_Hook}', "$BSHook") | Set-Content "./bmbfmod.json"
+    Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\bmbfmod.json" -DestinationPath "./Temp$ModID.zip" -Update
+    Move-Item "./Temp$ModID.zip" "./$ModID.zip" -Force
 }
 if ($? -And $args.Count -eq 0) {
 echo "Packaging QMod with ModID: $ModID"
     (Get-Content $modjson).replace('{VERSION_NUMBER_PLACEHOLDER}', "$VERSION") | Set-Content $modjson
     (Get-Content $modjson).replace('{BS_Hook}', "$BSHook") | Set-Content $modjson
     Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
-    Move-Item "./Temp$ModID.zip" "./$ModID.qmod" -Force
+    Move-Item "./Temp$ModID.zip" "./$ModID.zip" -Force
 }
 echo "Task Completed"
