ae93455163bd789528a06f21ab35827b5611df96
EnderdracheLP <30808481+EnderdracheLP@users.noreply.github.com>
2021-04-14 22:30:21 +0200

    Fixed build script for actions

--- build.ps1
+++ build.ps1
@@ -1,19 +1,22 @@
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 if ($args.Count -eq 0) {
 $ModID = "QuestSounds"
+$VERSION = "1.0.0"
 $BSHook = "1_3_3"
-$VERSION = "0.3.0"
+$codegen_ver = "0_7_1"
 }
 
 if ($args[0] -eq "--actions") {
     $ModID = $env:module_id
     $BSHook = $env:bs_hook
     $VERSION = $env:version
+    $codegen_ver = $env:codegen
 }
 echo "Building mod with ModID: $ModID version: $VERSION, BS-Hook version: $BSHook"
 Copy-Item "./Android_Template.mk" "./Android.mk" -Force
-(Get-Content "./Android.mk").replace('{BS_Hook}', "$BSHook") | Set-Content "./Android.mk"
-(Get-Content "./Android.mk").replace('{VERSION}', "$VERSION") | Set-Content "./Android.mk"
+(Get-Content "./Android.mk").replace('{BS_Hook}',   "$BSHook")        | Set-Content "./Android.mk"
+(Get-Content "./Android.mk").replace('{VERSION}',   "$VERSION")       | Set-Content "./Android.mk"
+(Get-Content "./Android.mk").replace('{CG_VER}',    "$codegen_ver")   | Set-Content "./Android.mk"
 
 $buildScript = "$NDKPath/build/ndk-build"
 if (-not ($PSVersionTable.PSEdition -eq "Core")) {
