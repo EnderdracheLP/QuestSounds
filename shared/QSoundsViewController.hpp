6f8ab6361ca525b78112128b6f4fdce05a87dedf
EnderdracheLP <30808481+EnderdracheLP@users.noreply.github.com>
2021-04-15 09:05:41 +0200

    Updated for newest custom-types

--- shared/QSoundsViewController.hpp
+++ shared/QSoundsViewController.hpp
@@ -6,7 +6,7 @@ DECLARE_CLASS_CODEGEN(QuestSounds, QSoundsViewController, HMUI::ViewController,
     DECLARE_OVERRIDE_METHOD(void, DidActivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidActivate", 3), bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
     DECLARE_OVERRIDE_METHOD(void, DidDeactivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidDeactivate", 2), bool removedFromHierarchy, bool systemScreenDisabling);
 
-    REGISTER_FUNCTION(QSoundsViewController,
+    REGISTER_FUNCTION(
         REGISTER_METHOD(DidActivate);
         REGISTER_METHOD(DidDeactivate);
     )
