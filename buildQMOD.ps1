321fbe6f604efd892fa1aaf7d881013a32d3b8f4
EnderdracheLP <enderdrachelp@gmail.com>
2021-08-06 16:40:40 +0200

    Fix buildQMOD for actions hopefully

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -2,7 +2,7 @@
 if ($args.Count -eq 0) {
 $ModID = "QuestSounds"
 $BSHook = "2_2_4"
-$VERSION = "1.0.0"
+$VERSION = "1.0.0-InDev"
 $BS_Version = "1.16.4"
 echo "Compiling Mod"
 & $PSScriptRoot/build.ps1
@@ -15,8 +15,9 @@ if ($args[0] -eq "--package") {
     $ModID = $env:module_id
     $BSHook = $env:bs_hook
     $BS_Version = $env:BSVersion
+    $VERSION = $env:version
 echo "Actions: Packaging QMod with ModID: $ModID and BS-Hook version: $BSHook"
-    (Get-Content "./mod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$env:version") | Set-Content "./mod.json"
+    (Get-Content "./mod.json").replace('{VERSION_NUMBER_PLACEHOLDER}', "$VERSION") | Set-Content "./mod.json"
     (Get-Content "./mod.json").replace('{BS_Hook}', "$BSHook") | Set-Content "./mod.json"
     (Get-Content "./mod.json").replace('{BS_Version}', "$BS_Version") | Set-Content "./mod.json"
     Compress-Archive -Path "./libs/arm64-v8a/lib$ModID.so", "./libs/arm64-v8a/libbeatsaber-hook_$BSHook.so", ".\Cover.jpg", ".\mod.json" -DestinationPath "./Temp$ModID.zip" -Update
