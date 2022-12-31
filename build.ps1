93c4487d7bd8798060b921c48d476479899f122e
EnderdracheLP <enderdrachelp@gmail.com>
2022-12-31 16:49:05 +0100

    Bump for Beat Saber 1.27.0_3631150051

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ echo "Arg $($i) is $($args[$i])"
 }
 if ($args.Count -eq 0 -or $actions -ne $true) {
 $ModID = "QuestSounds"
-$VERSION = "1.2.0"
+$VERSION = "1.4.0"
     if ($release -ne $true) {
         $VERSION += "-Dev"
     }
