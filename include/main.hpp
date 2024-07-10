d3aa9bbd3818a7e2a5a5e380ea887639a154c6ee
EnderdracheLP <enderdrachelp@elp.quest>
2024-07-10 21:47:15 +0200

    Update includes and logger, add export macro

--- include/main.hpp
+++ include/main.hpp
@@ -1,23 +1,24 @@
 #pragma once
 
+// Logging
+#include "paper/shared/Logger.hpp"
+
 // Include the modloader header, which allows us to tell the modloader which mod this is, and the version etc.
-#include "modloader/shared/modloader.hpp"
+#include "scotland2/shared/loader.hpp"
 
 // beatsaber-hook is a modding framework that lets us call functions and fetch field values from in the game
 // It also allows creating objects, configuration, and importantly, hooking methods to modify their values
-//#include "beatsaber-hook/shared/utils/utils.h"
 #include "beatsaber-hook/shared/utils/logging.hpp"
-//#include "beatsaber-hook/shared/utils/typedefs.h"
 #include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
 #include "beatsaber-hook/shared/config/config-utils.hpp"
-#if !defined(MAKE_HOOK_OFFSETLESS)
 #include "beatsaber-hook/shared/utils/hooking.hpp"
-#endif
 
 #include "System/Collections/IEnumerator.hpp"
 #include "custom-types/shared/coroutine.hpp"
 
 // Define these functions here so that we can easily read configuration and log information from other files
 Configuration& getConfig();
-Logger& getLogger();
+Paper::ConstLoggerContext<9UL> getLogger();
+
+#define QS_EXPORT extern "C" __attribute__((visibility("default")))
