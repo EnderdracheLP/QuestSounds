af73cdb695f0c3cd7021d19d3a70ee8a1e769314
EnderdracheLP <enderdrachelp@elp.quest>
2024-07-18 15:37:55 +0200

    Cleanup ObjectInstances.hpp

--- include/ObjectInstances.hpp
+++ include/ObjectInstances.hpp
@@ -1,16 +1,8 @@
 #pragma once
 #include "main.hpp"
-#include "QSoundsFlowCoordinator.hpp"
+// #include "QSoundsFlowCoordinator.hpp"
 
-//#include "GlobalNamespace/ResultsViewController.hpp"
 #include "GlobalNamespace/SongPreviewPlayer.hpp"
-//#include "GlobalNamespace/NoteCutSoundEffectManager.hpp"
-//#include "GlobalNamespace/NoteCutSoundEffect.hpp"
-//#include "GlobalNamespace/BasicUIAudioManager.hpp"
-//#include "GlobalNamespace/FireworkItemController.hpp"
-//#include "GlobalNamespace/GameServerLobbyFlowCoordinator.hpp"
-//#include "GlobalNamespace/MultiplayerModeSelectionFlowCoordinator.hpp"
-
 
 namespace QuestSounds::ObjectInstances {
 	extern GlobalNamespace::SongPreviewPlayer* SPP;
