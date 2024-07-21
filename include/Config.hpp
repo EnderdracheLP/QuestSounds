44372586f511d6902635aa1f7027ad12db08ecb2
EnderdracheLP <enderdrachelp@elp.quest>
2024-07-21 22:12:13 +0200

    Config, add missing initial values for some sounds

--- include/Config.hpp
+++ include/Config.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "scotland2/shared/loader.hpp"
 #include "rapidjson-macros/shared/macros.hpp"
 
@@ -7,7 +9,8 @@
 // #define CONFIG_VERSION_PRE_R "SoundsConfig_v1"
 // #define CONFIG_VERSION_LEGACY "Sounds"
 
-#define SOUND_VALUE(name, folder) VALUE_DEFAULT(Sound, name, Sound(soundPath + folder + "/", soundPath + folder + "/" + #name + ".ogg"))
+#define SOUND_VALUE(name, folder, ...) VALUE_DEFAULT(Sound, name, Sound(soundPath + folder + "/", soundPath + folder + "/" + #name + ".ogg", __VA_ARGS__))
+#define SOUND_VALUE_SIMPLE(name, folder) VALUE_DEFAULT(Sound, name, Sound(soundPath + folder + "/", soundPath + folder + "/" + #name + ".ogg"))
 
 // define an automatically serialized / deserialized instance variable with a custom name in the json file and a default value
 #pragma region NAMED_VALUE_CUSTOM(type, name, default, jsonName)
@@ -31,26 +34,26 @@ namespace QuestSounds {
     static const std::string soundPath = fmt::format(SOUND_PATH_FORMAT, modloader::get_application_id().c_str());
 
     DECLARE_JSON_CLASS(Sound,
-        NAMED_VALUE_DEFAULT(bool, Active, true, "activated");
         std::string FolderPath;
         std::string DefaultFilePath;
+        NAMED_VALUE_DEFAULT(bool, Active, true, "activated");
         NAMED_VALUE_CUSTOM(std::string, FilePath, DefaultFilePath, "filepath");
         NAMED_VALUE_OPTIONAL(float, VolumeOffset, "audioVolumeOffset");
         NAMED_VALUE_OPTIONAL(float, BeatOffset, "beatOffSet");
-        Sound(std::string folderPath, std::string defaultFilePath) : FolderPath(folderPath), DefaultFilePath(defaultFilePath) {}
+        Sound(std::string folderPath, std::string defaultFilePath, std::optional<float> volumeOffset = std::nullopt, std::optional<float> beatOffset = std::nullopt) : FolderPath(folderPath), DefaultFilePath(defaultFilePath), VolumeOffset(volumeOffset), BeatOffset(beatOffset) {}
     )
 
     DECLARE_JSON_CLASS(
         Sounds, 
-        SOUND_VALUE(HitSound, "HitSounds");
-        SOUND_VALUE(BadHitSound, "BadHitSounds");
-        SOUND_VALUE(NoteMissedSound, "NoteMissedSounds");
-        SOUND_VALUE(MenuMusic, "MenuMusic");
-        SOUND_VALUE(MenuClick, "MenuClicks");
-        SOUND_VALUE(Firework, "Fireworks");
-        SOUND_VALUE(LevelCleared, "LevelCleared");
-        SOUND_VALUE(LevelFailed, "LevelFailed");
-        SOUND_VALUE(LobbyMusic, "LobbyMusic");
+        SOUND_VALUE(HitSound, "HitSounds", 0, 0.185f);
+        SOUND_VALUE(BadHitSound, "BadHitSounds", 0);
+        SOUND_VALUE(NoteMissedSound, "NoteMissedSounds", 0);
+        SOUND_VALUE_SIMPLE(MenuMusic, "MenuMusic");
+        SOUND_VALUE_SIMPLE(MenuClick, "MenuClicks");
+        SOUND_VALUE_SIMPLE(Firework, "Fireworks");
+        SOUND_VALUE_SIMPLE(LevelCleared, "LevelCleared");
+        SOUND_VALUE(LevelFailed, "LevelFailed", 0);
+        SOUND_VALUE_SIMPLE(LobbyMusic, "LobbyMusic");
     )
 
     DECLARE_JSON_CLASS(
@@ -107,7 +110,8 @@ namespace QuestSounds {
 
 
 
-
+#undef NAMED_VALUE_CUSTOM
 #undef SOUND_PATH_FORMAT
+#undef SOUND_VALUE_SIMPLE
 #undef SOUND_VALUE
 // #undef CONFIG_SOUND
