15f8f33c01c361d7550348534e044c72b35e0c6f
EnderdracheLP <enderdrachelp@elp.quest>
2024-07-22 22:18:14 +0200

    Add name to config class, might need that

--- include/Config.hpp
+++ include/Config.hpp
@@ -9,8 +9,8 @@
 // #define CONFIG_VERSION_PRE_R "SoundsConfig_v1"
 // #define CONFIG_VERSION_LEGACY "Sounds"
 
-#define SOUND_VALUE(name, folder, ...) VALUE_DEFAULT(Sound, name, Sound(soundPath + folder + "/", soundPath + folder + "/" + #name + ".ogg", __VA_ARGS__))
-#define SOUND_VALUE_SIMPLE(name, folder) VALUE_DEFAULT(Sound, name, Sound(soundPath + folder + "/", soundPath + folder + "/" + #name + ".ogg"))
+#define SOUND_VALUE(name, folder, ...) VALUE_DEFAULT(Sound, name, Sound(#name, soundPath + folder + "/", soundPath + folder + "/" + #name + ".ogg", __VA_ARGS__))
+#define SOUND_VALUE_SIMPLE(name, folder) VALUE_DEFAULT(Sound, name, Sound(#name, soundPath + folder + "/", soundPath + folder + "/" + #name + ".ogg"))
 
 // define an automatically serialized / deserialized instance variable with a custom name in the json file and a default value
 #pragma region NAMED_VALUE_CUSTOM(type, name, default, jsonName)
@@ -34,13 +34,14 @@ namespace QuestSounds {
     static const std::string soundPath = fmt::format(SOUND_PATH_FORMAT, modloader::get_application_id().c_str());
 
     DECLARE_JSON_CLASS(Sound,
+        std::string Name;
         std::string FolderPath;
         std::string DefaultFilePath;
         NAMED_VALUE_DEFAULT(bool, Active, true, "activated");
         NAMED_VALUE_CUSTOM(std::string, FilePath, DefaultFilePath, "filepath");
         NAMED_VALUE_OPTIONAL(float, VolumeOffset, "audioVolumeOffset");
         NAMED_VALUE_OPTIONAL(float, BeatOffset, "beatOffSet");
-        Sound(std::string folderPath, std::string defaultFilePath, std::optional<float> volumeOffset = std::nullopt, std::optional<float> beatOffset = std::nullopt) : FolderPath(folderPath), DefaultFilePath(defaultFilePath), VolumeOffset(volumeOffset), BeatOffset(beatOffset) {}
+        Sound(std::string name, std::string folderPath, std::string defaultFilePath, std::optional<float> volumeOffset = std::nullopt, std::optional<float> beatOffset = std::nullopt) : Name(name), FolderPath(folderPath), DefaultFilePath(defaultFilePath), VolumeOffset(volumeOffset), BeatOffset(beatOffset) {}
     )
 
     DECLARE_JSON_CLASS(
