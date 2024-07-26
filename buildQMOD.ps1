d818dae13aaebd092dda3000c79058fa530c7476
EnderdracheLP <enderdrachelp@elp.quest>
2024-07-26 19:27:11 +0200

    Ensure cover image is actually included in qmod

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -66,7 +66,7 @@ $qmod = $qmodName + ".qmod"
 #     Move-Item $qmod $zip -Force
 # }
 
-qpm qmod zip -i ./build/ -i ./extern/libs/ -i ./Examples/ $qmod
+qpm qmod zip -i ./build/ -i ./extern/libs/ -f ./Cover.jpg -i ./Examples/ $qmod
 
 # Compress-Archive -Path $filelist -DestinationPath $zip -Update
 # Move-Item $zip $qmod -Force
